ed9ee334bba14bea8cd9bccb4d0ff1b527ca764c
Peppe Reres <peppereres@live.it>
2025-01-21 12:06:00 +0100

    Add new variables for Alessandro and Misilmeri in michele.js

--- michele.js
+++ michele.js
@@ -6,6 +6,12 @@ const hatedTeam = "Juve";
 
 const lazio = "Lazio";
 
+const alessandro = "Alessandro";
+
+const paese = "Misilmeri";
+
 console.log(
   `${michele} ${love} JavaScript! ${hatedTeam} sucks! and  ${lazio} is the best team in the world! `
 );
+
+console.log(`${alessandro} is from ${paese}`);
